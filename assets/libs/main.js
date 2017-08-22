492f4145f7e6c5dc7dc1bc5081a5f735887c426a
Wallef Antoine <antoine.wallef@gmail.com>
2017-08-22 17:19:46 +0200

    fullscreen

--- assets/libs/main.js
+++ assets/libs/main.js
@@ -237,9 +237,33 @@
   //////////////////////////////////////////////////////////////////////////////////
 
   launchRendering();
+
 }
 
 // boostrap trigger
 $(document).ready(init);
 
 })(window, jQuery);
+
+
+
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {
+      document.documentElement.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+}
